b7571b5587462a1b16e1fc4cd75c5faab43d61fe
vladhrytcev <vlad.hrytsev.work@gmail.com>
2021-01-14 11:35:44 +0200

    pdf link fix

--- file-upload-frontend/src/utils/downloadAction.js
+++ file-upload-frontend/src/utils/downloadAction.js
@@ -1,6 +1,9 @@
 export const downloadAction = (downloadUrl, fileName) => {
+  const ext = fileName.split('.').pop().slice(0,3)
   const link = document.createElement("a");
   link.href = `${process.env.REACT_APP_DOWNLOAD_URL}${downloadUrl}`;
+
+  if(ext === 'pdf') link.target = '_blank'
   link.download = fileName;
   document.body.appendChild(link);
   link.click();
