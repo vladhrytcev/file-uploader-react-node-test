bb9549a5f27395b04a11ad547858a884aa3cd172
vladhrytcev <vlad.hrytsev.work@gmail.com>
2021-02-04 10:21:04 +0200

    download test

--- file-upload-frontend/src/utils/downloadAction.js
+++ file-upload-frontend/src/utils/downloadAction.js
@@ -1,14 +1,16 @@
 export const downloadAction = (downloadUrl, fileName) => {
-  const ext = fileName.split('.').pop()
-  const link = document.createElement("a");
-  link.href = `${process.env.REACT_APP_DOWNLOAD_URL}${downloadUrl}`;
+  // const ext = fileName.split('.').pop()
+  // const link = document.createElement("a");
+  // link.href = `${process.env.REACT_APP_DOWNLOAD_URL}${downloadUrl}`;
 
-  console.log(ext)
-  console.log(`${process.env.REACT_APP_DOWNLOAD_URL}${downloadUrl}`)
-  console.log(fileName)
-  if(ext === 'pdf') link.target = '_blank'
-  link.download = fileName;
-  document.body.appendChild(link);
-  link.click();
+  window.open(`${process.env.REACT_APP_DOWNLOAD_URL}${downloadUrl}`, '_blank')
+
+  // console.log(ext)
+  // console.log(`${process.env.REACT_APP_DOWNLOAD_URL}${downloadUrl}`)
+  // console.log(fileName)
+  // if(ext === 'pdf') link.target = '_blank'
+  // link.download = fileName;
+  // document.body.appendChild(link);
+  // link.click();
   // link.remove();
 };
