7a00611bc0edfa936c694acf715f1179b646f8b5
vladhrytcev <vlad.hrytsev.work@gmail.com>
2021-02-04 10:52:05 +0200

    download fix

--- file-upload-frontend/src/utils/downloadAction.js
+++ file-upload-frontend/src/utils/downloadAction.js
@@ -1,16 +1,7 @@
 export const downloadAction = (downloadUrl, fileName) => {
-  // const ext = fileName.split('.').pop()
-  // const link = document.createElement("a");
-  // link.href = `${process.env.REACT_APP_DOWNLOAD_URL}${downloadUrl}`;
-
-  window.open(`${process.env.REACT_APP_DOWNLOAD_URL}${downloadUrl}`, '_blank')
-
-  // console.log(ext)
-  // console.log(`${process.env.REACT_APP_DOWNLOAD_URL}${downloadUrl}`)
-  // console.log(fileName)
-  // if(ext === 'pdf') link.target = '_blank'
-  // link.download = fileName;
-  // document.body.appendChild(link);
-  // link.click();
-  // link.remove();
+  const ext = fileName.split('.').pop()
+  const url = `${process.env.REACT_APP_DOWNLOAD_URL}${downloadUrl}`;
+  if(ext === 'pdf') {
+    window.open(url, '_blank')
+  } else window.open(url)
 };
